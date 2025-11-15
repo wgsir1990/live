1f96c23254f3a8ee0b11fa992e194ea361dd1147
wgsir1990 <16259322+wgsir1990@user.noreply.gitee.com>
2025-11-15 17:48:47 +0000

    update js/抓饭体育.js.
    Signed-off-by: wgsir1990 <16259322+wgsir1990@user.noreply.gitee.com>

--- js/抓饭体育.js
+++ js/抓饭体育.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'抓饭体育',
     // host:'https://www.zhuafan.xin',
-    host:'https://www.zhuafantiyu.co',
+    host:'https://cn-zhuafan-zhibo.net/',
     url:'/sports-home/category/fyclass',
     class_name:'全部&足球&篮球&网球&羽乒&台球&排球&棒球&搏击&电竞&棋盘&户外&聊天&原声&综合',
     class_url:'all&Football&Basketball&Tennis&Badminton&Billiards&Volleyball&Baseball&Wrestling&Popular&Boardgame&Outdoors&Chat&Acoustic&Others',
