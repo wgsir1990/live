19624058b15993ea31714d41d269a377a12f22ed
wgsir1990 <wgsir1990@163.com>
2026-06-07 18:43:23 +0800

    Update JRKAN直播.js

--- js/JRKAN直播.js
+++ js/JRKAN直播.js
@@ -1,24 +1,13 @@
-// 道长 drpy仓库 https://gitcode.net/qq_32394351/dr_py
-// 道长 drpy安卓本地搭建说明 https://code.gitlink.org.cn/api/v1/repos/hjdhnx/dr_py/blob/master/%E5%AE%89%E5%8D%93%E6%9C%AC%E5%9C%B0%E6%90%AD%E5%BB%BA%E8%AF%B4%E6%98%8E.md
-// 道长 drpy写源 模板规则说明 https://gitcode.net/qq_32394351/dr_py#%E6%A8%A1%E6%9D%BF%E8%A7%84%E5%88%99%E8%AF%B4%E6%98%8E
-// 道长 drpy写源 套模模版 https://ghproxy.net/https://raw.githubusercontent.com/hjdhnx/dr_py/main/js/%E6%A8%A1%E6%9D%BF.js
-// 道长 drpy写源 相关视频教程 https://www.youtube.com/watch?v=AK7cN-fcwm4
-// 道长 drpy写源 写源教学视频 https://t.me/fongmi_offical/54080/63553
-// 海阔下载 https://haikuo.lanzoui.com/u/GoldRiver
-// 影视TV 官方TG Drpy群 https://t.me/fongmi_offical/63689
-// 影视TV 官方TG 下载 https://t.me/fongmi_release
-
-
 var rule = {
     title:'JRKAN直播',
-    host:'https://www.jrskk.com/',
-    // JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com
-    // JRKAN网址发布:qiumi1314.com
+    host:'http://m.jrkan2023.com/',
+    // JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com /Jrkan备用域名1: www.jrkan2022.com 备用域名2: www.jrs23.com 备用域名2: www.jrskk.com 最新网址发布:www.qiumi1314.co
+    
     url:'/fyclass',
     searchUrl:'',
     searchable:0,
     quickSearch:0,
-    class_name:'全部',
+    class_name:'影视JRKAN直播赛事',
     class_url:'/',
     //class_url:'?live',
     headers:{
@@ -37,7 +26,7 @@ var rule = {
         "img":"img&&src",
         "desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
         "content":".sub_list ul&&Text",
-        "tabs":"js:TABS=['JRKAN直播']",
+        "tabs":"js:TABS=['赛事直播']",
         "lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
     },
     搜索:'',
